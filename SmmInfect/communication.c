ffcdfc52bcea476d25aba36b90f1247ba0f24f8f
r1cky33 <rickiecee@gmail.com>
2025-05-22 19:13:06 +0200

    rewritten the communication protocol to process commands

--- SmmInfect/communication.c
+++ SmmInfect/communication.c
@@ -1,21 +1,23 @@
 #include "communication.h"
-#include "Library/BaseMemoryLib.h"
-#include "Uefi/UefiBaseType.h"
-#include "windows.h"
+#include "logging.h"
 #include "memory.h"
+#include "windows.h"
+
+#include <Library/BaseMemoryLib.h>
 #include <Library/UefiLib.h>
+#include <PiSmm.h>
 #include <Protocol/SmmCpu.h>
 #include <Uefi.h>
-#include <Protocol/SmmCpu.h>
-#include <PiSmm.h>
+#include <Uefi/UefiBaseType.h>
 #include <stddef.h>
-#include <string.h>
-#include "serial.h"
 
 UINT64 SmiCountIndex = 0;
+UINT64 cprocess = 0;
+UINT64 base = 0;
+UINT64 section = 0;
+UINT64 processCr3 = 0;
 
-UINT64 GetCommunicationProcess()
-{
+UINT64 GetCommunicationProcess() {
     UINT64 proc = GetWindowsEProcess("ricky.exe");
     return proc;
 }
@@ -30,87 +32,100 @@ EFI_STATUS PerformCommunication() {
         return EFI_NOT_READY;
     }
 
-    SERIAL_PRINT("[INFO] kernel: 0x%x\r\n", kernel);
-    SERIAL_PRINT("[INFO] cr3: 0x%x\r\n", cr3);
+    UINT64 tempProc = GetCommunicationProcess();
+    if (cprocess != tempProc) {
+        cprocess = tempProc;
+        if (!cprocess) {
+            return EFI_NOT_FOUND;
+        }
 
-    UINT64 cprocess = GetCommunicationProcess();
-    if (!cprocess) {
-        return EFI_NOT_FOUND;
+        LOG_INFO("[INFO] cprocess: 0x%x\r\n", cprocess);
+
+        base = GetWindowsBaseAddressModuleX64(cprocess, L"ricky.exe");
+        if (!base) {
+            return EFI_NOT_FOUND;
+        }
+
+        LOG_INFO("[INFO] cprocess base: 0x%x\r\n", base);
+        section =
+            GetWindowsSectionBaseAddressX64(cprocess, base, (UINT8 *)".RICKY");
+        if (!section) {
+            return EFI_NOT_FOUND;
+        }
+
+        LOG_INFO("[INFO] cprocess section: 0x%x\r\n", section);
+
+        processCr3 = GetWindowsProcessCr3(cprocess);
+        if (!processCr3) {
+            return EFI_NOT_FOUND;
+        }
+
+        LOG_INFO("[INFO] cprocess cr3: 0x%x\r\n", processCr3);
     }
-
-    SERIAL_PRINT("[INFO] cprocess: 0x%x\r\n", cprocess);
-
-    UINT64 base = GetWindowsBaseAddressModuleX64(cprocess, L"ricky.exe");
-    if (!base) {
-        return EFI_NOT_FOUND;
-    }
-
-    SERIAL_PRINT("[INFO] cprocess base: 0x%x\r\n", base);
-    UINT64 section = GetWindowsSectionBaseAddressX64(cprocess, base, (UINT8*)".RICKY");
-    if (!section) {
-        return EFI_NOT_FOUND;
-    }
-
-    SERIAL_PRINT("[INFO] cprocess section: 0x%x\r\n", section);
 
     SmmCommunicationProtocol protocol = {0};
-    ReadVirtual(section + 0b0, GetWindowsProcessCr3(cprocess), (UINT8*)&protocol, sizeof(protocol));
+    ReadVirtual(section + 0b0, processCr3, (UINT8 *)&protocol,
+                sizeof(protocol));
 
     if (protocol.magic != SMM_PROTOCOL_MAGIC) {
         return EFI_INVALID_PARAMETER;
     }
 
-    *(UINT64*)TranslateVirtualToPhysical(GetWindowsProcessCr3(cprocess), section + offsetof(SmmCommunicationProtocol, smi_count)) = SmiCountIndex;
+    *(UINT64 *)CachedVTOP(
+        processCr3, section + offsetof(SmmCommunicationProtocol, smi_count)) =
+        SmiCountIndex;
 
     switch (protocol.command) {
-        case CMD_READ_VIRTUAL_MEMORY: {
+    case CMD_READ_VIRTUAL_MEMORY: {
 
-            SERIAL_PRINT("[INFO] CMD_READ_VIRTUAL_MEMORY");
-            UINT64 tprocess = GetWindowsEProcess((CHAR8*)protocol.read.process_name);
-            if (!tprocess) {
-                return EFI_NOT_FOUND;
-            }
-
-            SERIAL_PRINT("[INFO] tprocess base: 0x%x\r\n", tprocess);
-
-            if (protocol.read.size > sizeof(protocol.buffer)) {
-                return EFI_BAD_BUFFER_SIZE;
-            }
-
-            ReadVirtual(protocol.read.address, GetWindowsProcessCr3(tprocess), protocol.buffer, protocol.read.size);
-
-
-            SERIAL_PRINT("[INFO] read: 0x%x  copying...\r\n", protocol.read.address);
-            // write back results
-            UINT64 buffer_phys = TranslateVirtualToPhysical(GetWindowsProcessCr3(cprocess), section + offsetof(SmmCommunicationProtocol, buffer));
-            CopyMem((VOID*)buffer_phys, protocol.buffer, protocol.read.size);
-            break;
+        LOG_INFO("[INFO] CMD_READ_VIRTUAL_MEMORY \r\n");
+        UINT64 tprocess =
+            GetWindowsEProcess((CHAR8 *)protocol.read.process_name);
+        if (!tprocess) {
+            return EFI_NOT_FOUND;
         }
 
-        case CMD_GET_MODULE_BASE: {
+        LOG_VERB("[INFO] tprocess base: 0x%x\r\n", tprocess);
 
-            SERIAL_PRINT("[INFO] CMD_READ_VIRTUAL_MEMORY");
-            UINT64 tprocess = GetWindowsEProcess((CHAR8*)protocol.module.target_process);
-            if (!tprocess) {
-                return EFI_NOT_FOUND;
-            }
-
-    
-            SERIAL_PRINT("[INFO] tprocess base: 0x%x\r\n", tprocess);
-            
-            UINT64 module_base = GetWindowsBaseAddressModuleX64(tprocess, protocol.module.target_module);
-
-            if (!module_base) {
-                return EFI_NOT_FOUND;
-            }
-
-            SERIAL_PRINT("[INFO] module base: 0x%x\r\n", module_base);
-
-            // write back base address
-            UINT64 buffer_phys = TranslateVirtualToPhysical(GetWindowsProcessCr3(cprocess),
-                                                         section + offsetof(SmmCommunicationProtocol, buffer));
-            *(UINT64*)buffer_phys = module_base;
-            break;
+        if (protocol.read.size > sizeof(protocol.buffer)) {
+            LOG_ERROR("[ERR] buffer too large: 0x%x \r\n", protocol.read.size);
+            return EFI_BAD_BUFFER_SIZE;
         }
+
+        ReadVirtual(protocol.read.address, GetWindowsProcessCr3(tprocess),
+                    protocol.buffer, protocol.read.size);
+
+        LOG_VERB("[INFO] read: 0x%x  copying...\r\n", protocol.read.address);
+
+        UINT64 buffer_phys = CachedVTOP(
+            processCr3, section + offsetof(SmmCommunicationProtocol, buffer));
+        CopyMem((VOID *)buffer_phys, protocol.buffer, protocol.read.size);
+        break;
     }
+
+    case CMD_GET_MODULE_BASE: {
+
+        LOG_INFO("[INFO] CMD_READ_VIRTUAL_MEMORY \r\n");
+        UINT64 tprocess =
+            GetWindowsEProcess((CHAR8 *)protocol.module.target_process);
+        if (!tprocess) {
+            return EFI_NOT_FOUND;
+        }
+
+        LOG_VERB("[INFO] tprocess base: 0x%x\r\n", tprocess);
+
+        UINT64 module_base = GetWindowsBaseAddressModuleX64(
+            tprocess, protocol.module.target_module);
+
+        if (!module_base) {
+            return EFI_NOT_FOUND;
+        }
+
+        LOG_VERB("[INFO] module base: 0x%x\r\n", module_base);
+
+        UINT64 buffer_phys = CachedVTOP(
+            processCr3, section + offsetof(SmmCommunicationProtocol, buffer));
+        *(UINT64 *)buffer_phys = module_base;
+        break;
+    }}
 }
--- SmmInfect/communication.h
+++ SmmInfect/communication.h
@@ -1,32 +1,32 @@
-#pragma once 
-#include <Uefi.h>
+#pragma once
 #include <PiSmm.h>
 #include <Protocol/SmmCpu.h>
+#include <Uefi.h>
 #define SMM_PROTOCOL_MAGIC 'i'
 
 #define CMD_READ_VIRTUAL_MEMORY 0x10
-#define CMD_GET_MODULE_BASE     0x20
+#define CMD_GET_MODULE_BASE 0x20
 
 #pragma pack(1)
 typedef struct _SmmCommunicationProtocol {
-    UINT8 magic;                    // offset 0
-    UINT8 command;                  // offset 1
+    UINT8 magic;   // offset 0
+    UINT8 command; // offset 1
     union {
         // For CMD_READ_VIRTUAL_MEMORY
         struct {
-            UINT8  process_name[30];  // ASCII target process name
-            UINT64 address;           // Absolute address to read
-            UINT64 size;              // Read size
+            UINT8 process_name[30]; // ASCII target process name
+            UINT64 address;         // Absolute address to read
+            UINT64 size;            // Read size
         } read;
-        
+
         // For CMD_GET_MODULE_BASE
         struct {
-            UINT8  target_process[30]; // ASCII process name
+            UINT8 target_process[30]; // ASCII process name
             UINT16 target_module[30]; // UTF-16 module name (like original)
         } module;
     };
-    UINT8  buffer[512];             // Enlarged read buffer
-    UINT64 smi_count;               // SMI counter
+    UINT8 buffer[512]; // Enlarged read buffer
+    UINT64 smi_count;  // SMI counter
 } SmmCommunicationProtocol;
 #pragma pack()
 
